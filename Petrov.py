137ec2ec082e8e9847fa86f6bb4e1a1a11af6cbd
Ssnikers <90687265+Ssnikers@users.noreply.github.com>
2021-11-18 20:14:49 +0300

    Update Petrov.py

--- Petrov.py
+++ Petrov.py
@@ -1,1 +1,14 @@
+import random
+from time import sleep
+from tqdm import tqdm
 
+init()
+
+name2 = input("Номер тел/URL: ")
+input("Прокси: ")
+name = input("Сколько сообщений/ботов: ")
+print("Атака!")
+for i in tqdm(range(100), colour="green"):
+	sleep(random.uniform(0.01, 0.1))
+
+input() 
